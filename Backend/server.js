f2dd45f3004a3064c3cbe24a7aa3b1d10524043f
Aniket Agrawal <ankt7987@gmail.com>
2025-09-16 21:43:48 +0530

    User-model & DB setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const app = require('./src/app');
+const connectDb = require("./src/db/db");
+const httpServer = require("http").createServer(app);
 
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
+connectDb()
+
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
