cbe19a9bba9deea88b8f7206ce3b5b2f0cc7c159
Aniket Agrawal <ankt7987@gmail.com>
2025-09-17 00:44:12 +0530

    Gemini Integration, Authentication & Socket

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
+/* Routes */
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+/* Using Routes */
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
